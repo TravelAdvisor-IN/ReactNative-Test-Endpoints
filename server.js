ccc5a294f3f2153226b8959ec6562ee6038f17dd
Chethan S <cchethans14@gmail.com>
2023-04-30 14:45:23 +0530

    server

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+const filePath = path.join(__dirname, 'homepage.json');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
+  if (req.url === '/' && req.method === 'GET') {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Internal Server Error');
+        return;
+      }
+
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.end('Not Found');
+  }
 });
 
-const port = process.env.PORT || 3000;
-
 server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  const address = server.address();
+  console.log(`Server running at http://${address.address}:${address.port}`);
 });
